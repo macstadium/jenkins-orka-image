c7a112fa4b8a20f403797c5683a9875f1d2e4670
Ivan Spasov <frankeys89@gmail.com>
2019-10-29 16:58:39 +0200

    Pipeline

---
+++ init.groovy
@@ -0,0 +1,35 @@
+import jenkins.model.Jenkins;
+
+Jenkins.instance.createProjectFromXML("Orka Pipeline", new ByteArrayInputStream("""\
+<?xml version='1.1' encoding='UTF-8'?>
+<flow-definition plugin="workflow-job">
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps">
+    <script>pipeline {
+    agent { label &apos;orka&apos; }
+    stages {
+        stage(&apos;install&apos;) {
+            steps {
+                sh &apos;echo Installing dependencies!&apos;
+            }
+        }
+        stage(&apos;build&apos;) {
+            steps {
+                sh &apos;echo Building on: \$(uname -a)&apos;
+            }
+        }
+         stage(&apos;test&apos;) {
+            steps {
+                sh &apos;echo Testing!&apos;
+            }
+        }
+    }
+}
+</script>
+    <sandbox>true</sandbox>
+  </definition>
+  <triggers/>
+  <disabled>false</disabled>
+</flow-definition>
+""".bytes))
